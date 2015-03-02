7ce924b9e3a592984b31568f98ceffbf6cae7394
jinsong bai <bjstry@163.com>
2015-03-02 11:11:45 +0800

    1532-1111-first

---
+++ README.md
@@ -0,0 +1,14 @@
+SpeekFrame 
+
+steep 1 
+create index.php file
+
+steeup 2
+edit index.php
+
+define('PRJ','Home');
+require './Speek/Speek.php';
+
+ Author Jinsong
+Version BETA3
+ Update 2015-03-02 11:04
---
+++ Speek.php
@@ -0,0 +1,18 @@
+<?php
+	//beta2 start 15210-1026
+	//author TRY
+
+	defined('SYS') or define('SYS',__DIR__);
+	defined('PRJ') or define('PRJ',dirname($_SERVER['SCRIPT_FILENAME']).'/SpeekHome');
+	defined('EXT') or define('EXT','.php');
+	defined('CEXT') or define('CEXT','.class.php');
+	defined('SYS_CORE') or define('SYS_CORE',SYS.'/Core/');
+	defined('SYS_CONF') or define('SYS_CONF',SYS.'/Conf/');
+	defined('SYS_LIB') or define('SYS_LIB',SYS.'/Lib/');
+	defined('SYS_LOG') or define('SYS_LOG',SYS.'/Log/');
+	defined('SYS_VERSION') or define('SYS_VERSION','SpeekFrame-0.03');
+	require SYS_LIB.'/functions.php';
+	require SYS_CORE.'/Speek.class.php';
+	require SYS_LIB.'Tpl/Smarty'.CEXT;
+	Speek::Run();
+?>
